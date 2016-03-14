40470d0a40352066db4e993b998a57638bb9b088
guillaume <guillaume.kaba@external.engie.com>
2016-03-14 17:08:39 +0100

    update plugin path

--- public/iframe_vis.js
+++ public/iframe_vis.js
@@ -1,9 +1,9 @@
 define(function(require) {
   // we need to load the css ourselves
-  require('plugins/iframe_vis/iframe_vis.less');
+  require('plugins/kibana-iframe-vis/iframe_vis.less');
 
   // we also need to load the controller and used by the template
-  require('plugins/iframe_vis/iframe_vis_controller');
+  require('plugins/kibana-iframe-vis/iframe_vis_controller');
 
   // register the provider with the visTypes registry so that other know it exists
   require('ui/registry/vis_types').register(IframeVisProvider);
@@ -18,9 +18,9 @@ define(function(require) {
       title: 'Iframe widget',
       icon: 'fa-code',
       description: 'Useful for displaying aweb page into an iframe.',
-      template: require('plugins/iframe_vis/iframe_vis.html'),
+      template: require('plugins/kibana-iframe-vis/iframe_vis.html'),
       params: {
-        editor: require('plugins/iframe_vis/iframe_vis_params.html')
+        editor: require('plugins/kibana-iframe-vis/iframe_vis_params.html')
       },
       requiresSearch: false
     });
